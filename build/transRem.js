5da89064eca00d9328c40ce1bc1e2c27e325a479
pagnkelly <632228198@qq.com>
2023-05-30 11:31:05 +0800

    feat: init

---
+++ build/transRem.js
@@ -0,0 +1,21 @@
+const fs = require('node:fs/promises')
+const path = require('node:path')
+
+const resolve = file => path.resolve(__dirname, '..', file)
+
+const files = [resolve('lib/index.js')]
+
+function transRem(content) {
+  return content.replace(/(\d+(\.\d+)?)rem/g, (match, p1) => `${p1 * 37.5}rpx`)
+}
+
+async function doBuild(files) {
+  await Promise.all(files.map(async (file) => {
+    const content = await fs.readFile(file, { encoding: 'utf8' })
+    await fs.writeFile(file, transRem(content))
+  }))
+}
+
+doBuild(files).catch((e) => {
+  console.error(e)
+})
